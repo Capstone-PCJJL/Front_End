c809bd0516941ba098634c29ef5c8f13a55f076f
dleren <dleren@uwaterloo.ca>
2025-06-09 12:14:47 -0600

    working api to mysql database

---
+++ server.js
@@ -0,0 +1,36 @@
+import mysql from 'mysql';
+import config from './config.js';
+import fetch from 'node-fetch';
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import bodyParser from 'body-parser';
+import response from 'express';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 5500;
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.post('/api/getMovies', (req, res) => {
+	const connection = mysql.createConnection(config);
+  
+	const sql = 'SELECT * FROM movie_capstone_db.checkpoints';
+	connection.query(sql, (error, results) => {
+	  if (error) {
+		console.error(error.message);
+		res.status(500).send('Error fetching movies');
+		return;
+	  }
+	  res.json(results);
+	});
+  
+	connection.end();
+  });
+
+  app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const testApiCall = async () => {
+      try {
+        const response = await fetch('/api/getMovies', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (!response.ok) {
+          throw new Error(`Error: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        console.log('API Response:', data);
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    };
+
+    testApiCall();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Welcome to our Capstone Project
         </p>
