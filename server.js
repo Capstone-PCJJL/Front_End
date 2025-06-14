27d76dc461987631c4e6feb938774358cb990029
Jojo Ngai <jojo.ngai2003@gmail.com>
2025-06-14 17:32:28 -0400

    updated server file to debug proxy issue

--- server.js
+++ server.js
@@ -1,35 +1,31 @@
-import mysql from 'mysql';
-import config from './config.js';
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import bodyParser from 'body-parser';
-import response from 'express';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const mysql = require('mysql');
+const config = require('./config.js');
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 5500;
+
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.post('/api/getMovies', (req, res) => {
-	const connection = mysql.createConnection(config);
-  
-	const sql = 'SELECT * FROM movie_capstone_db.movies_raw LIMIT 20;';
-	connection.query(sql, (error, results) => {
-	  if (error) {
-		console.error(error.message);
-		res.status(500).send('Error fetching movies');
-		return;
-	  }
-	  res.json(results);
-	});
-  
-	connection.end();
+  const connection = mysql.createConnection(config);
+
+  const sql = 'SELECT * FROM movie_capstone_db.movies_raw LIMIT 20;';
+  connection.query(sql, (error, results) => {
+    if (error) {
+      console.error(error.message);
+      res.status(500).send('Error fetching movies');
+      return;
+    }
+    res.json(results);
   });
 
-  app.listen(port, () => console.log(`Listening on port ${port}`));
+  connection.end();
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
